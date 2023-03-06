c565a51ff25750f4015e962387eb218bdb98767b
Chris <chris@headbonecreative.com>
2023-03-05 21:59:24 -0700

    moving platform with sine wave in x direction

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
+#define _USE_MATH_DEFINES 1
+#include <math.h>
 
 // Sets default values
 AMovingPlatform::AMovingPlatform()
@@ -16,9 +17,9 @@ void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
-//    MyVector = GetActorLocation();
-//    MyVector = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
-//    SetActorLocation(MyVector);
+    startPosition = GetActorLocation();
+    positionOffset = FVector::ZeroVector;
+    yDirection = 1.0f;
 }
 
 // Called every frame
@@ -26,8 +27,39 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-    MyVector = GetActorLocation();
-    MyVector.Y = MyVector.Y + 1;
-    SetActorLocation(MyVector);
+    const float cycleTime = M_PI * 2;       // time to complete one sine wave (in seconds)
+    float cycleSpeed = M_PI * 2 / cycleTime;
+    FVector deltaMove = FVector::ZeroVector;
+
+    // Move platform forward
+    curvePosition += DeltaTime * cycleSpeed;
+    if (curvePosition >= M_PI * 2)
+    {
+        curvePosition -= M_PI * 2;
+    }
+
+    float xMove = sin(curvePosition) * PlatformVelocity.X;
+    deltaMove.X = abs(xMove);
+
+    float yMove = positionOffset.Y + PlatformVelocity.Y * DeltaTime * yDirection;
+    deltaMove.Y = abs(yMove);
+    if (yMove >= targetYOffset)
+    {
+        float adjustment = yMove - targetYOffset;
+        yMove = yMove - adjustment * 2;
+        yDirection = -1.0f;
+    }
+    else if (yMove <= 0.0f)
+    {
+        yMove = -yMove;
+        yDirection = 1.0f;
+    }
+
+    positionOffset.X = xMove;
+    positionOffset.Y = yMove;
+    SetActorLocation(startPosition + positionOffset);
+
+    PlatformMoveDistance = GetActorLocation() - startPosition;
+    DistanceMoved += FVector::Distance(FVector::ZeroVector, deltaMove);
 }
 
--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -22,10 +22,20 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category = "Moving Platform")
+	FVector PlatformVelocity = FVector(100, 100, 0);
 
-    UPROPERTY(EditAnywhere)
-    FVector MyVector = FVector(0, 0, 0);
+	UPROPERTY(VisibleAnywhere, Category = "Moving Platform")
+	FVector PlatformMoveDistance = FVector(0, 0, 0);
 
-    UPROPERTY(EditAnywhere)
-    float MyX = 0;
+	UPROPERTY(VisibleAnywhere, Category = "Moving Platform")
+	float DistanceMoved = 0.0f;
+
+private:
+	FVector startPosition;
+	FVector positionOffset;
+	float curvePosition;
+	float targetYOffset = 3000.0f;
+	float xDirection = 0.0f;
+	float yDirection = 0.0f;
 };
